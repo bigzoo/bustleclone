582516daf1c45afc6e1d3106f3213ff5fa28b592
Chris Nyaga <nyagachris75@gmail.com>
2017-06-07 16:19:13 +0300

    Admin

--- app/components/update-article.js
+++ app/components/update-article.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+//  showUpdate:false
+  // toggleView:false,
+  // actions:{
+  //
+  // }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,18 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  show: true,
+  //toggleView: true,
   model(){
     return Ember.RSVP.hash({
       toparticles:this.store.findAll('toparticle'),
-      articles:this.store.findAll('article');
+      articles:this.store.findAll('article')
     });
-  }
+  },
   actions: {
     saveParent(params){
       //console.log('fired from child component');
       //debugger;
       var newArticle = this.store.createRecord('article',params);
       newArticle.save();
+    },
+    showUpdate(){
+      //this.set('toggleView',true);
+      //alert('click me');
     }
   }
 });
